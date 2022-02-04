2a04ac7b187f3c7fce21fc20f8b97d26bb0fff55
NeirouzJbira <jbira.neirouz@gmail.com>
2022-02-04 09:04:05 +0100

    init

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,11 +1,7 @@
 import Card from "../UI/Card";
 
-function AboutUs () {
-    return (
-        <Card>
-            
-        </Card>
-    );
+function AboutUs() {
+  return <Card></Card>;
 }
 
 export default AboutUs;
