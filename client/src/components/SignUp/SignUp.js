aa5121e6c173f1bdc3ba09d37f4f279697871c9d
ramigargouri <ramigargouri@gmail.com>
2022-02-07 21:00:18 +0100

    init

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -7,7 +7,9 @@ import Card from "../UI/Card";
 const SignUp = () => {
   const history = useNavigate();
 
-  const [username, setusername] = useState("");
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [phone, setphone] = useState(0);
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
   const [confim, setconfim] = useState("");
@@ -21,13 +23,11 @@ const SignUp = () => {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(username);
-    console.log(email);
-    console.log(password);
-    console.log(confim);
     axios
       .post("http://localhost:8000/api/register", {
-        username,
+        firstname,
+        lastname,
+        phone,
         email,
         password,
         confirm: confim,
@@ -48,7 +48,7 @@ const SignUp = () => {
     <Card className="Register">
       <form onSubmit={onSubmitHandler}>
         <h1>Register </h1>
-        <br/>
+        <br />
         <label htmlFor="username" className="form-label">
           Username
         </label>
@@ -68,7 +68,14 @@ const SignUp = () => {
           <input
             type="text"
             className="form-control"
-            onChange={(e) => setusername(e.target.value)}
+            onChange={(e) => setfirstname(e.target.value)}
+            id="username"
+            required
+          />
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setlastname(e.target.value)}
             id="username"
             required
           />
@@ -78,6 +85,30 @@ const SignUp = () => {
         ) : (
           ""
         )}
+        <label htmlFor="phone" className="form-label">
+          Phone Number
+        </label>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-telephone"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+            </svg>
+          </span>
+          <input
+            type="number"
+            className="form-control"
+            onChange={(e) => setphone(e.target.value)}
+            id="phone"
+            required
+          />
+        </div>
         <label htmlFor="email" className="form-label">
           Email
         </label>
