798b9f2089c52c309cef4bc0854acdfde8f42965
NeirouzJbira <jbira.neirouz@gmail.com>
2022-02-11 10:42:12 +0100

    init

--- server/config/jwt.js
+++ server/config/jwt.js
@@ -3,7 +3,7 @@ const secret = "this is secret";
 
 module.exports.secret = secret;
 module.exports.authenticate = (req, res, next) => {
-  console.log(req.cookies)
+  console.log(req.cookies);
   jwt.verify(req.cookies.usertoken, secret, (err, payload) => {
     if (err) {
       res.status(401).json({ verified: false });
