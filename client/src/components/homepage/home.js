040c67089d920074b22092989c9a2355552d2702
NeirouzJbira <jbira.neirouz@gmail.com>
2022-02-15 21:53:23 +0100

    init

--- client/src/components/homepage/home.js
+++ client/src/components/homepage/home.js
@@ -1,23 +1,63 @@
 import React from "react";
 import "./home.css";
-import Card from "../UI/Card";
-import Seach from "../Search/Search";
+import SearchFormHotel from "../Search/SearchForm/SearchFormHotel";
+import Promos from "../PromosHotel/Promos";
 
 function Home() {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <Card className="home">
+      <div className="row row1">
+        <div className="container">
+          <div className="row row2">
+            <h1 className="title">Enjoy your trip </h1>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <SearchFormHotel />
+          </div>
+        </div>
+      </div>
+      <div className="row ">
+        <div className="container">
           <br />
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-12 col-md-8 col-xl-5">
-                <Seach />
-              </div>
+          <br />
+          <br />
+          <br />
+
+          <div className="row row3">
+            <div className="col-4">
+              <img
+                src="https://img.icons8.com/external-filled-outline-satawat-anukul/100/000000/external-summer-summer-filled-outline-filled-outline-satawat-anukul-43.png"
+                alt=""
+              />{" "}
+              Best destinations
+            </div>
+            <div className="col-4">
+              <img
+                src="https://img.icons8.com/fluency/100/000000/card-wallet.png"
+                alt=""
+              />{" "}
+              Best prices{" "}
+            </div>
+            <div className="col-4">
+              <img
+                src="https://img.icons8.com/external-itim2101-lineal-color-itim2101/100/000000/external-support-services-contact-and-message-itim2101-lineal-color-itim2101-2.png"
+                alt=""
+              />
+              Best services
             </div>
           </div>
           <br />
-        </Card>
+          <br />
+          <br />
+        </div>
+      </div>
+      <div className="row">
+        <Promos />
       </div>
     </div>
   );
