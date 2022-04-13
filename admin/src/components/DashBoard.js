9d2923c26f1e052847bc0bae2447d38899e33d97
ramigargouri <ramigargouri@gmail.com>
2022-04-13 04:04:36 +0100

    12/04/2022

--- admin/src/components/DashBoard.js
+++ admin/src/components/DashBoard.js
@@ -1,47 +1,33 @@
 import PromosOverview from "./PromosOverview";
 import ReservationsOverview from "./ReservationOverview";
 import UsersOverview from "./UsersOverview";
-import ListUsers from "./ListUsers";
-import ListHotels from "./ListHotels";
-import ListEvents from "./ListEvents";
 function DashBoard() {
   return (
-    <div className="container-fluid" style={{ marginTop: "120px" }}>
-      <div className="row" style={{ backgroundColor: "beige" }}>
+    <div className="container-fluid">
+      <div className="row" style={{ minHeight: window.innerHeight }}>
         <div
           className="container"
-          style={{ marginTop: "50px", marginBottom: "50px" }}
+          style={{ marginTop: "auto", marginBottom: "auto" }}
         >
           <div className="row">
-            <div className="col-4">
+            <div className="col-2"></div>
+            <div className="col-3">
               <UsersOverview />
             </div>
-            <div className="col-4">
+            <div className="col-2"></div>
+            <div className="col-3">
               <PromosOverview />
             </div>
+            <div className="col-1"></div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-4"></div>
             <div className="col-4">
               <ReservationsOverview />
             </div>
+            <div className="col-4"></div>
           </div>
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <ListUsers />
-            </div>
-          </div>
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-6">
-          <ListHotels />
-        </div>
-        <div className="col-6">
-          <ListEvents />
         </div>
       </div>
     </div>
